98a657779ad6e332aab1da423b4d269b23f3c3ca
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 21:59:54 +0100

    issue an ajax request by wire the SearchBar to the action creator fetchWeather by connecting to mapDispatchToProps

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,19 @@
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
+
+
 import React from 'react';
 import { Component } from 'react';
 
-export default class SearchBar extends Component {
+ class SearchBar extends Component {
     constructor(props){
         super(props);
 
         this.state = {term: ''};
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
@@ -17,8 +23,10 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault();
-        
+
         // we need to fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({term:''});
     }
 
     render(){
@@ -39,3 +47,9 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
